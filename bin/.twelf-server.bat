253c503a047c6bddece52a26df0aa4eb90aba6f4
Florian Rabe <florian.rabe@gmail.com>
2009-11-04 13:57:25 +0000

    git-svn-id: https://svn.concert.cs.cmu.edu/twelf/branches/twelf-mod@1642 92263adb-174b-0410-95c5-c49bc0650b85

--- bin/.twelf-server.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-SET SML=sml
-SET TWELFDIR=C:\Program Files\Twelf\
-%SML% @SMLload="%TWELFDIR%bin\.heap\twelf-server"
---
+++ bin/twelf-server.bat
@@ -0,0 +1,3 @@
+@echo off
+SET TWELFBIN=%~dp0
+sml @SMLload="%TWELFBIN%\.heap\twelf-server"
