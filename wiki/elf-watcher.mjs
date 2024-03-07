c65b2e8fab93601270ff4aaff89df5cab6a51a45
Rob Simmons <robsimmons@gmail.com>
2024-03-07 15:24:35 -0500

    Fix deployment errors

--- wiki/elf-watcher.mjs
+++ wiki/elf-watcher.mjs
@@ -1,9 +1,13 @@
 import { exec } from "child_process";
-import { existsSync, readdirSync, watch } from "fs";
+import { existsSync, mkdir, mkdirSync, readdirSync, watch } from "fs";
 import { argv } from "process";
 
 const DIR_OF_ELF = "src/content/twelf";
 const DIR_OF_MDX = "src/content/docs/wiki";
+if (!existsSync(DIR_OF_MDX)) {
+  mkdirSync("src/content/docs/wiki");
+}
+
 function mdxOfFile(file) {
   const elfname = `${DIR_OF_ELF}/${file}`;
   if (!file.endsWith(".elf") || !existsSync(elfname)) return;
@@ -23,10 +27,8 @@ for (const file of readdirSync(DIR_OF_ELF)) {
   mdxOfFile(file);
 }
 if (argv[2] === "--watch") {
-    console.log(
-        `elf->mdx watching ${DIR_OF_ELF} for changes.`
-      );
-        watch(DIR_OF_ELF, (_change, file) => {
+  console.log(`elf->mdx watching ${DIR_OF_ELF} for changes.`);
+  watch(DIR_OF_ELF, (_change, file) => {
     mdxOfFile(file);
   });
 }
