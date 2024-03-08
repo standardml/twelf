92e7edb9a4311987e836d2d237bd6590731615a1
Rob Simmons <robsimmons@gmail.com>
2024-03-07 21:00:27 -0500

    Attempt to run tests again

--- TEST/regression-wiki.mjs
+++ TEST/regression-wiki.mjs
@@ -6,7 +6,7 @@ for (const file of readdirSync(WIKI_TWELF_LOC)) {
   if (file.endsWith(".elf")) {
     const cfg = file.slice(0, file.length - 4) + ".cfg";
     writeFileSync(WIKI_TWELF_LOC + "cfg", "file");
-    cfgs.push(cfg);
+    cfgs.push(`test ${WIKI_TWELF_LOC}${cfg}`);
   }
 }
 
