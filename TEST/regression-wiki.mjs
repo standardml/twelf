2c8ba3ff6920a28ffdfd12c1db0c6719319208ac
Rob Simmons <robsimmons@gmail.com>
2024-03-07 20:54:40 -0500

    Autogenerate wiki twelf

---
+++ TEST/regression-wiki.mjs
@@ -0,0 +1,13 @@
+import { readdirSync, writeFileSync } from "fs";
+
+const WIKI_TWELF_LOC = "../wiki/src/content/twelf/";
+const cfgs = [];
+for (const file of readdirSync(WIKI_TWELF_LOC)) {
+  if (file.endsWith(".elf")) {
+    const cfg = file.slice(0, file.length - 4) + ".cfg";
+    writeFileSync(WIKI_TWELF_LOC + "cfg", "file");
+    cfgs.push(cfg);
+  }
+}
+
+writeFileSync("regression-wiki.txt", cfgs.join("\n"));
