bc2606b071dd0b8c8c955dd80f219406878f0f4e
Rob Simmons <robsimmons@gmail.com>
2024-03-31 12:58:20 -0400

    Figure out how to do dynamic paths correctly

--- wiki/src/pages/twelf-wasm/index.ts
+++
@@ -1,8 +0,0 @@
-export const prerender = false;
-export const GET = async () => {
-  return new Response(
-    await fetch(`https://jcreedcmu.github.io/twelf-wasm/`).then((response) =>
-      response.blob()
-    )
-  );
-};
